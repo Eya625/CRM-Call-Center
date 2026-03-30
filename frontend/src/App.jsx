ef72ca00eb940ec27c01b7c8d327605a9defbd78
Eya <boukadida.eya15@gmail.com>
2026-03-30 14:01:46 +0100

    ajout login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout";
 import Manager from "./pages/manager";
 import Admin from "./pages/admin";
+import Login from "./pages/Login";
+import Agent from "./pages/Agent";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Page de login hors Layout */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Toutes les autres pages avec le Layout */}
         <Route element={<Layout />}>
-          <Route path="/" element={<Navigate to="/manager" replace />} />
+          <Route path="/agent" element={<Agent />} />
+          <Route path="/" element={<Navigate to="/login" replace />} />{" "}
+          {/* redirige vers login */}
           <Route path="/manager" element={<Manager />} />
           <Route path="/admin" element={<Admin />} />
         </Route>
@@ -17,13 +24,3 @@ export default function App() {
     </BrowserRouter>
   );
 }
-=======
-import React from "react";
-import AppRouter from "./routes/AppRouter";
-
-function App() {
-  return <AppRouter />;
-}
-
-export default App;
->>>>>>> e341b5ec (Mise à jour du dialplan Asterisk et configuration PJSIP/1002)
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,23 +1,9 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "antd/dist/reset.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
->>>>>>> e341b5ec (Mise à jour du dialplan Asterisk et configuration PJSIP/1002)
